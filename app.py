9c4ae53478c4c0299f1e74ab3a09a120da9af50d
Stefano Mersi <stefano.mersi@gmail.com>
2021-05-31 12:17:58 +0200

    Now addding a new rounte (address) to display the posts and linking these adresses from the main page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 # returns a database connnection to the local sqlite3 file
 def get_db_connection():
@@ -7,8 +8,19 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+# gets a single post from the database
+def get_post(post_id):
+    conn = get_db_connection()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+    if post is None:
+        abort(404)
+    return post
+
 app = Flask(__name__)
 
+# the main page (index.html)
 @app.route('/')
 def index():
     # gets the database connection, selects all posts and closes the connection
@@ -17,4 +29,11 @@ def index():
     conn.close()
     # renders the page as "index.html" (which must use the "posts" variable)
     return render_template('index.html', posts=posts)
+
+# the page of a single post
+@app.route('/<int:post_id>')
+def post(post_id):
+    # gets the post (defined above) and renders it via the "post.html" page and the variable "post" used thereby
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
 
