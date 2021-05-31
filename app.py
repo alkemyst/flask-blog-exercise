2bba8ab14929e15d74ec263394c437a9be6a097d
Stefano Mersi <stefano.mersi@gmail.com>
2021-05-31 12:13:36 +0200

    Addding the list of posts from the main page

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
+import sqlite3
 from flask import Flask, render_template
+
+# returns a database connnection to the local sqlite3 file
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # gets the database connection, selects all posts and closes the connection
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    # renders the page as "index.html" (which must use the "posts" variable)
+    return render_template('index.html', posts=posts)
 
